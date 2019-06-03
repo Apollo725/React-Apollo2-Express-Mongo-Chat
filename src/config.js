1a5cd35b8f755c0647cc428fba6f559a03e61f22
Apollo <38185119+Apollo725@users.noreply.github.com>
2019-06-03 14:36:11 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const {
   APP_PORT = 4000,
   NODE_ENV = 'development',
   DB_USERNAME = 'apollo725',
-  DB_PASSWORD = 'Test123!',
+  DB_PASSWORD = 'xxxxxxx',
   DB_HOST = 'ds137863.mlab.com',
   DB_PORT = '37863',
   DB_NAME = 'chat'
